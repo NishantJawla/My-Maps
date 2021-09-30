53d1ff73387ee9f38794634322a62b32704ec83d
NishantJawla13 <nishantjawla12225@gmail.com>
2021-09-30 10:44:44 +0530

    --create::created graphql server

---
+++ resolvers.js
@@ -0,0 +1,11 @@
+const user = {
+    _id:"771257",
+    name:"User",
+    email:"user@gmail.com",
+    picture:"https://i.pinimg.com/564x/ea/55/37/ea5537b36fffd087e4e884c5572504ad.jpg"
+}
+module.exports = {
+    Query: {
+        me: () => user
+    }
+}
