998a9c802047627175f1a02fc288be4c9330530b
NishantJawla13 <nishantjawla12225@gmail.com>
2021-10-01 21:16:51 +0530

    Added Connection With Mongodb

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const {ApolloServer} = require('apollo-server');
-
+const moongose = require('mongoose');
+require('dotenv').config();
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers')
-
+moongose.connect(process.env.MONGO_URI,  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }).then(()=>{
+      console.log("Database Connected!!");
+  }).catch(() => {
+      console.log("Error In Mongodb Connection")
+  })
 const server = new ApolloServer ({
     typeDefs,
     resolvers
