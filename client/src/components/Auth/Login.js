d6ae6cabfac60a3469f4b1b2f7f8614b7e70b2b4
NishantJawla13 <nishantjawla12225@gmail.com>
2021-10-08 19:41:54 +0530

    added google login button to the frontend

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
-
+import { GoogleLogin } from "react-google-login";
 const Login = ({ classes }) => {
-	return <div>Login</div>;
+	const onSuccess = googleUser => {
+		const idToken = googleUser.getAuthResponse().id_token;
+		console.log(idToken);
+	};
+	return (
+		<GoogleLogin
+			clientId="746933482383-ac3p828g0200smabv61efb8de6b4kanp.apps.googleusercontent.com"
+			onSuccess={onSuccess}
+			isSignedIn={true}
+		/>
+	);
 };
 
 const styles = {
