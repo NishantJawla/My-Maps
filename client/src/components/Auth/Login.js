43dba28f8eb13b21651b047729b88e9dab958ce0
NishantJawla13 <nishantjawla12225@gmail.com>
2021-10-14 03:10:21 +0530

    cleaned the architecture and rewritten several files for best coding practices

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,35 +1,50 @@
 import React, { useContext } from "react";
 import { withStyles } from "@material-ui/core/styles";
-// import Typography from "@material-ui/core/Typography";
+import Typography from "@material-ui/core/Typography";
 import { GoogleLogin } from "react-google-login";
 import { GraphQLClient } from "graphql-request";
 import Context from "../../context";
-const ME_QUERY = `{
-	me {
-	  _id
-	  name
-	  email
-	  picture
-	}
-  }`;
+import { ME_QUERY } from "../../graphql/queries";
 const Login = ({ classes }) => {
 	const { dispatch } = useContext(Context);
 	const onSuccess = async googleUser => {
-		const idToken = googleUser.getAuthResponse().id_token;
-		const client = new GraphQLClient("http://localhost:4000/graphql", {
-			headers: {
-				authorization: idToken
-			}
-		});
-		const { me } = await client.request(ME_QUERY);
-		dispatch({ type: "LOGIN_USER", payload: me });
+		try {
+			const idToken = googleUser.getAuthResponse().id_token;
+			const client = new GraphQLClient("http://localhost:4000/graphql", {
+				headers: {
+					authorization: idToken
+				}
+			});
+			const { me } = await client.request(ME_QUERY);
+			dispatch({ type: "LOGIN_USER", payload: me });
+		} catch (err) {
+			onFailure(err);
+		}
 	};
+
+	const onFailure = err => {
+		console.error("Error Logging IN!!!!", err);
+	};
+
 	return (
-		<GoogleLogin
-			clientId="746933482383-ac3p828g0200smabv61efb8de6b4kanp.apps.googleusercontent.com"
-			onSuccess={onSuccess}
-			isSignedIn={true}
-		/>
+		<div className={classes.root}>
+			<Typography
+				component="h1"
+				variant="h3"
+				gutterBottom
+				noWrap
+				style={{ color: rgb(66, 133, 244) }}
+			>
+				WELCOME
+			</Typography>
+			<GoogleLogin
+				clientId="746933482383-ac3p828g0200smabv61efb8de6b4kanp.apps.googleusercontent.com"
+				onSuccess={onSuccess}
+				onFailure={onFailure}
+				isSignedIn={true}
+				theme="dark"
+			/>
+		</div>
 	);
 };
 
